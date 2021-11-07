cedd2ea4c6aa50d9da300acb213e90d69877f496
Andrew <Pf1bym1rf1>
2021-11-07 10:51:11 +0300

    hw02 change

--- hw02-generics/src/main/java/hw/CustomerService.java
+++ hw02-generics/src/main/java/hw/CustomerService.java
@@ -7,19 +7,20 @@ public class CustomerService {
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
-    Map <Customer, String> customers = new HashMap<>();
+    final Map <Customer, String> customers = new HashMap<>();
 
     public Map.Entry<Customer, String> getSmallest() {
-
-        return customers.entrySet().stream().min((entry1, entry2) -> entry1.getKey().getScores() > entry2.getKey().getScores() ? 1 : -1).get();
+        Map<Customer, String> customersCopy = copy();
+        return customersCopy.entrySet().stream().min((entry1, entry2) -> entry1.getKey().getScores() > entry2.getKey().getScores() ? 1 : -1).get();
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
 
+        Map<Customer, String> customersCopy = copy();
         Customer returnCustomer = null;
         Map.Entry<Customer, String> e = null;
 
-        for (Map.Entry<Customer, String> entry : customers.entrySet()) {
+        for (Map.Entry<Customer, String> entry : customersCopy.entrySet()) {
 
             if (!entry.getKey().equals(customer) && entry.getKey().getScores()>customer.getScores()) {
                 if (returnCustomer==null) {
@@ -38,5 +39,13 @@ public class CustomerService {
 
     public void add(Customer customer, String data) {
         customers.put(customer,data);
+    }
+
+    private Map<Customer, String> copy() {
+        Map<Customer, String> customerCopy = new HashMap<Customer, String>();
+        for(Customer key : customers.keySet()) {
+            customerCopy.put(new Customer(key.getId(), key.getName(), key.getScores()), customers.get(key));
+        }
+        return customerCopy;
     }
 }
